553f100a1616f4495a558a769881d405494b2075
hasonateto12 <122304609+hasonateto12@users.noreply.github.com>
2024-10-29 20:18:59 +0200

    Update index.js
    עשיתי משתנה בשם pointsRoutes שמכיל את הCrud

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ const port = 4225;
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-app.use(cors.static(path.join(__dirname, 'public')));  // Serve static files from public directory
+app.use(express.static(path.join(__dirname, 'public')));  // Serve static files from public directory
 
-const pointsRoutes = require('./jsedit/PointsEdit');
+// Import the points routes
+const pointsRoutes = require('./public/js/PointsEdit');  // Adjust the path if necessary
+
+// Use the points routes
 app.use('/points', pointsRoutes);
 
 app.listen(port, () => {
