e1f9c4e83633b6d5f0b4499d8fc769076ed869cb
hasonateto12 <122304609+hasonateto12@users.noreply.github.com>
2024-10-30 00:30:05 +0200

    Update PointsEdit.js
    עשיתי דף של Crud לנקודות והגדרתי ראוותר לכל פעולה

--- public/js/PointsEdit.js
+++ public/js/PointsEdit.js
@@ -1,43 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
-let points = [];
+let points=[
+    {name:"Point1",location:1},
+    {name:"Point2", location:2},
+];
 
+router.post('/Point', (req, res) => {
+    let point={};
+    point.name=req.body.name;
+    point.location =req.body.location ;
+    points.push(point);
+
+    res.status(200).json("ok");
+});
+router.patch('/Point', (req, res) => {
+    let idx=req.body.idx;
+    let point={};
+    point.name  =req.body.full_name;
+    point.location=req.body.location;
+   points[idx]=point;
+    res.status(200).json("ok");
+});
+router.delete('/Point', (req, res) => {
+    let idx=req.body.idx;
+    points.splice(idx, 1);
+
+    res.status(200).json("ok");
+})
 router.get('/Point', (req, res) => {
     res.status(200).json(points);
 });
 
+module.exports = router;
 
-router.post('/Point', (req, res) => {
-    let point = {
-        name: req.body.name,
-        location: req.body.location
-    };
-    points.push(point);
-    res.status(200).json("ok");
-});
-
-
-router.patch('/Point', (req, res) => {
-    let idx = req.body.idx;  
-    if (idx < points.length) {
-        points[idx].name = req.body.full_name;  
-        points[idx].location = req.body.location;  
-        res.status(200).json("ok");
-    } else {
-        res.status(404).json("Point not found");
-    }
-});
-
-
-router.delete('/Point', (req, res) => {
-    let idx = req.body.idx; 
-    if (idx < points.length) {
-        points.splice(idx, 1);
-        res.status(200).json("ok");
-    } else {
-        res.status(404).json("Point not found");
-    }
-});
-
-module.exports = router;  
